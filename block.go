f7eec30d7d139b33f0b5201aae1259ca26ee9845
ma2ma <steve2008.ma@gmail.com>
2018-08-30 16:54:11 +0800

    add segment intersect with triangle function

--- block.go
+++ block.go
@@ -1,24 +1,54 @@
 package block
 
 import (
+	"fmt"
+
 	"github.com/ma2ma/gola"
 )
 
 var Epsilon float64 = 0.0001
 
-func IntersectTriangle(orig, dir, v0, v1, v2 *gola.Vector3) (intersect bool, t, u, v float64) {
+type Segment struct {
+	a *gola.Vector3
+	b *gola.Vector3
+}
+type Ray struct {
+	orig *gola.Vector3
+	dir  *gola.Vector3
+}
+type Triangle struct {
+	v0 *gola.Vector3
+	v1 *gola.Vector3
+	v2 *gola.Vector3
+}
+
+func SegmentXTriangle(seg Segment, tri Triangle) (intersect bool, focus *gola.Vector3) {
+	dir := seg.b.NewSub(seg.a)
+	segmentLength := dir.Length()
+	dir.Normalize()
+
+	fmt.Println(seg.a, seg.b)
+	intersect, t, _, _ := IntersectTriangle(Ray{orig: seg.a, dir: dir}, tri)
+	if t > segmentLength {
+		intersect = false
+	}
+	focus = seg.a.NewAdd(dir.Scale(t))
+	return
+}
+
+func IntersectTriangle(ray Ray, tri Triangle) (intersect bool, t, u, v float64) {
 	intersect = false
 	t, u, v = 0, 0, 0
-	e1 := v1.NewSub(v0)
-	e2 := v2.NewSub(v0)
-	p := dir.NewCross(e2)
+	e1 := tri.v1.NewSub(tri.v0)
+	e2 := tri.v2.NewSub(tri.v0)
+	p := ray.dir.NewCross(e2)
 	det := e1.Dot(p)
 
 	var vt *gola.Vector3
 	if det > 0 {
-		vt = orig.NewSub(v0)
+		vt = ray.orig.NewSub(tri.v0)
 	} else {
-		vt = v0.NewSub(orig)
+		vt = tri.v0.NewSub(ray.orig)
 		det = -det
 	}
 	// fmt.Println(det)
@@ -33,13 +63,13 @@ func IntersectTriangle(orig, dir, v0, v1, v2 *gola.Vector3) (intersect bool, t, 
 		return
 	}
 	q := vt.NewCross(e1)
-	v = dir.Dot(q)
+	v = ray.dir.Dot(q)
 	// fmt.Println(v)
 	if v < 0.0 || u+v > det {
 		return
 	}
 	t = e2.Dot(q)
-	// fmt.Println(t)
+	fmt.Println(t)
 	if t < 0.0 {
 		return
 	}
@@ -47,6 +77,7 @@ func IntersectTriangle(orig, dir, v0, v1, v2 *gola.Vector3) (intersect bool, t, 
 	t *= finvdet
 	u *= finvdet
 	v *= finvdet
+	fmt.Println(t)
 	intersect = true
 	return
 }
--- block_test.go
+++ block_test.go
@@ -6,6 +6,27 @@ import (
 	"github.com/ma2ma/gola"
 )
 
+func TestSegmentXTriangle(t *testing.T) {
+	a := &gola.Vector3{0, 0, 0}
+	b := &gola.Vector3{10, 0, 0}
+	b_un := &gola.Vector3{0.5, 0, 0}
+	v0 := &gola.Vector3{1, 1, -1}
+	v1 := &gola.Vector3{1, -1, -1}
+	v2 := &gola.Vector3{1, 0, 1}
+	expected := true
+	actual, focus := SegmentXTriangle(Segment{a: a, b: b}, Triangle{v0: v0, v1: v1, v2: v2})
+	actual_un, _ := SegmentXTriangle(Segment{a: a, b: b_un}, Triangle{v0: v0, v1: v1, v2: v2})
+
+	if actual != expected {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
+	if !focus.Equals(&gola.Vector3{1, 0, 0}) {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
+	if actual_un == expected {
+		t.Errorf("Expected %v, got %v", expected, actual_un)
+	}
+}
 func TestIntersectTriangle(t *testing.T) {
 	orig := &gola.Vector3{0, 0, 0}
 	orig_un := &gola.Vector3{10, 0, 0}
@@ -14,10 +35,14 @@ func TestIntersectTriangle(t *testing.T) {
 	v0 := &gola.Vector3{1, 1, -1}
 	v1 := &gola.Vector3{1, -1, -1}
 	v2 := &gola.Vector3{1, 0, 1}
+	tri := Triangle{v0: v0, v1: v1, v2: v2}
+	test := Ray{orig: orig, dir: dir}
+	test1 := Ray{orig: orig_un, dir: dir}
+	test2 := Ray{orig: orig_un2, dir: dir}
 	expected := true
-	actual, _, _, _ := IntersectTriangle(orig, dir, v0, v1, v2)
-	actual_un, _, _, _ := IntersectTriangle(orig_un, dir, v0, v1, v2)
-	actual_un2, _, _, _ := IntersectTriangle(orig_un2, dir, v0, v1, v2)
+	actual, _, _, _ := IntersectTriangle(test, tri)
+	actual_un, _, _, _ := IntersectTriangle(test1, tri)
+	actual_un2, _, _, _ := IntersectTriangle(test2, tri)
 	if actual != expected {
 		t.Errorf("Expected %v, got %v", expected, actual)
 	}
